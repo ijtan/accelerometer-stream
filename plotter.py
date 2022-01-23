7273bb8222505a5f591f9985e0e69f3ea54ad907
Ethan Zammit <ethanzammit@gmail.com>
2022-01-23 12:37:02 +0100

    messy implementatyion; most most of the time, motion works now

--- plotter.py
+++ plotter.py
@@ -2,24 +2,42 @@
 import json
 import os
 import matplotlib.pyplot as plt
-file_path = 'plots.json'
-plt_mot_history = {'x': [], 'y': [], 'z': []}
-plt_or_history = {'alpha': [], 'beta': [], 'gamma': []}
+
+plt_mot_history = {'x': [], 'y': [], 'z': [], 'time': []}
+plt_or_history = {'alpha': [], 'beta': [], 'gamma': [], 'time': []}
+
+motion_path = 'Motion.json'
+orientation_path = 'Orientation.json'
+
+
+def get_data():
+    plt_or_history = []
+    plt_mot_history = []
+    try:
+        with open(motion_path, 'r') as f:
+            plt_mot_history = json.load(f)
+        with open(orientation_path, 'r') as f:
+            plt_or_history = json.load(f)
+    except FileNotFoundError:
+        print('File not found')
+    except json.decoder.JSONDecodeError:
+        print('JSON error')
+
+    return plt_mot_history, plt_or_history
 
 while True:
-    if os.path.exists(file_path):
-        with open(file_path, 'r') as f:
-            plt.clf()
-            try:
-                plt_or_history = json.load(f)
-            except json.decoder.JSONDecodeError:
-                continue
-            plt.plot(plt_or_history['alpha'], label='alpha')
-            plt.plot(plt_or_history['beta'], label='beta')
-            plt.plot(plt_or_history['gamma'], label='gamma')
-            plt.legend()
-            plt.draw()
-            plt.pause(0.00001)
-    else:
-        print('no file')
-        exit()
+    motion_data, orientation_data = get_data()
+    # print(motion_data.keys())
+    # print(orientation_data.keys())
+
+    plt.subplot(2, 1, 1)
+    plt.plot(motion_data['time'], motion_data['x'], 'r')
+    plt.plot(motion_data['time'], motion_data['y'], 'g')
+    plt.plot(motion_data['time'], motion_data['z'], 'b')
+    plt.subplot(2, 1, 2)
+    plt.plot(orientation_data['time'], orientation_data['alpha'], 'r')
+    plt.plot(orientation_data['time'], orientation_data['beta'], 'g')
+    plt.plot(orientation_data['time'], orientation_data['gamma'], 'b')
+    plt.pause(0.001)
+    plt.draw()
+    plt.clf()
--- static/motion.js
+++ static/motion.js
@@ -14,7 +14,24 @@ const checkIfGyroIsEnabled = async () => {
   });
 };
 
-document.getElementById("deviceorientationdata").innerHTML = "test";
+const checkIfMotionIsEnabled = async () => {
+  // alert("checking mototo");
+  return new Promise((resolve) => {
+    const id = setTimeout(() => {
+      document.getElementById("nomotion").style.display = "block";
+      resolve(false);
+    }, 500);
+    const resolveMotionCheck = () => {
+      clearTimeout(id);
+      resolve(true);
+      document.getElementById("nomotion").style.display = "none";
+      window.removeEventListener("devicemotion", resolveMotionCheck);
+      // alert('Motion wohoo');
+    };
+    window.addEventListener("devicemotion", resolveMotionCheck);
+    // alert("Motion give pls");
+  });
+};
 
 //open websocket
 const ws = new WebSocket("wss://192.168.0.2:8000/ws");
@@ -24,65 +41,81 @@ ws.onopen = () => {
 };
 
 
+const batch_motion = [];
+const batch_motion_size = 10;
+// const batch_motion_interval = 1000;
+const batch_orientation = [];
+const batch_orientation_size = 10;
+// const batch_orientation_interval = 1000;
+
+sent = 0;
+function sendData(data, label){
+  labelled = {
+    data: data,
+    label: label
+  };
+  const data_str = JSON.stringify(labelled);
+  ws.send(data_str);
   
-continue_send = true;
+  batch_motion.length = 0;
+  batch_orientation.length = 0;
+
+  sent += 1;
+  document.getElementById("sent").innerHTML = sent.toString();
+}
+
+
+
+
+
 function updateDeviceOrientationData(e) {
-  if(!continue_send) return;
-  // only send every 5th data point
 
   const dataText = `alpha: ${e.alpha} <br>
     beta: ${e.beta} <br>
     gamma: ${e.gamma} <br>`;
   document.getElementById("deviceorientationdata").innerHTML = dataText;
 
-  //jsonify it
+  //objextify it
   const data = {
     alpha: e.alpha,
     beta: e.beta,
     gamma: e.gamma,
+    time: e.timeStamp,
   };
 
+  batch_orientation.push(data);
+  if(batch_orientation.length >= batch_orientation_size){
+    sendData(batch_orientation, "orientation");
+    data = null;
+    batch_orientation = [];
 
-
-  const data_str = JSON.stringify(data);
-
-  ws.send(data_str);
-  // continue_send = false;
-
-  // wait for reply from servee
-  // ws.onmessage = (event) => {
-  //   console.log(event.data);
-  //   continue_send = true;
-  // };
+  }
   
 };
 
 async function updateDeviceMotion(e){
-  if(!continue_send) return;
-  // only send every 5th data point
 
-  const dataText = `x: ${e.acceleration.x} <br>
-    y: ${e.acceleration.y} <br>
-    z: ${e.acceleration.z} <br>`;
-  document.getElementById("devicemotiondata").innerHTML = dataText;
+  // const dataText = `x: ${e.acceleration.x} <br>
+  //   y: ${e.acceleration.y} <br>
+  //   z: ${e.acceleration.z} <br>`;
+  // document.getElementById("devicemotiondata").innerHTML = dataText;
 
-  //jsonify it
+  //objectify it
+  // alert('Motion');
+  // alert(e);
   const data = {
-    x: e.acceleration.x,
-    y: e.acceleration.y,
-    z: e.acceleration.z,
+    x: e.accelerationIncludingGravity.x,
+    y: e.accelerationIncludingGravity.y,
+    z: e.accelerationIncludingGravity.z,
+    time: e.timeStamp,
   };
 
-  const data_str = JSON.stringify(data);
-
-  ws.send(data_str);
-  // continue_send = false;
-
-  // wait for reply from servee
-  // ws.onmessage = (event) => {
-  //   console.log(event.data);
-  //   continue_send = true;
-  // };
+  batch_motion.push(data);
+  if(batch_motion.length >= batch_motion_size){
+    sendData(batch_motion, "motion");
+    data = null;
+    batch_motion = [];
+  }
   
 }
 
@@ -90,15 +123,7 @@ window.addEventListener("deviceorientation", updateDeviceOrientationData);
 // window.addEventListener("devicemotion", updateDeviceMotion);
 
 
-const showNoDeviceOrientationMessage = () => {
-  document.getElementById("nodeviceorientation").style.display = "block";
-};
-
-const showDeviceOrientationMessage = () => {
-  document.getElementById("deviceorientation").style.display = "block";
-};
-
-const requestAccess = async (e) => {
+const requestOrientationAccess = async (e) => {
   if (
     typeof DeviceOrientationEvent === "undefined" ||
     !DeviceOrientationEvent.requestPermission
@@ -116,15 +141,44 @@ const requestAccess = async (e) => {
   });
 };
 
+const requestMotionAccess = async (e) => {
+  alert('Will Request Motion Access');
+  if (
+    typeof DeviceMotionEvent !== "undefined" &&
+    typeof DeviceMotionEvent.requestPermission === "function"
+  ) {
+    // (optional) Do something before API request prompt.
+    DeviceMotionEvent.requestPermission()
+      .then((response) => {
+        // (optional) Do something after API prompt dismissed.
+        if (response == "granted") {
+          alert(`permission: granted!!`);
+          return permission === "granted";
+          // window.addEventListener("devicemotion", (e) => {
+          //   // do something for 'e' here.
+          //   return permission=== "granted";
+          // });
+        }
+      })
+      .catch(console.error);
+  } else {
+    alert("DeviceMotionEvent is not defined");
+  }
+}
+
 document
   .getElementById("requestaccess")
   .addEventListener("click", async (e) => {
     try {
-      const gotAccess = await requestAccess(e);
-      alert(`gotAccess: ${gotAccess}`);
+      const nodevicemotion = await requestMotionAccess(e);
+      // alert(`gotMotionbAccess: ${gotMotionAccess}`);
+
+      const gotOrientationAccess = await requestOrientationAccess(e);
+      alert(`gotOrientationAccess: ${gotOrientationAccess}`);
+
       init();
     } catch (e) {
-      alert(e);
+      alert('Error: '+e);
     }
   });
 
@@ -139,7 +193,24 @@ const init = async () => {
     return;
   }
 
-  showDeviceOrientationMessage();
+
+
+  const isMotionEnabled = await checkIfMotionIsEnabled();
+  if (!isMotionEnabled) {
+    // showNoDeviceMotionMessage();
+    alert('No Motion');
+    return;
+  }else{
+    // alert('Motion all good');
+    window.addEventListener("devicemotion", updateDeviceMotion);
+  }
+
+  // showDeviceOrientationMessage();
+  // showDeviceMotionMessage();W
 };
+
+function sayGotMoto() {
+  alert("got a new moto");
+}
 
 init();
